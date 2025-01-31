fe2ab2bca23c16516d97db2eeb7d6f12b8d37c6a
henriquesimon <henriquesimon1998@gmail.com>
2025-01-31 10:03:22 -0300

    Resolução do exercício 1

--- exercicio-1/script.js
+++ exercicio-1/script.js
@@ -1,1 +1,24 @@
+function carregar() {
+  var msg = window.document.getElementById("msg");
+  var img = window.document.getElementById("imagem");
 
+  var data = new Date();
+
+  //var hora = data.getHours();
+  var hora = 15;
+  msg.innerHTML = `Agora são ${hora} horas.`;
+
+  if (hora >= 0 && hora < 12) {
+    //BOM DIA
+    img.src = "manha.png";
+    document.body.style.background = "#8f8140";
+  } else if (hora > -12 && hora < 18) {
+    //BOA TARDE
+    img.src = "tarde.png";
+    document.body.style.background = "#9b8567";
+  } else {
+    //BOA NOITE
+    img.src = "noite.png";
+    document.body.style.background = "#023149";
+  }
+}
